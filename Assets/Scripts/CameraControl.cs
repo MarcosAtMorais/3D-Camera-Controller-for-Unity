d00bc8f3e09bdf8b90572dc26803ed1bd117c420
Marcos Morais <marcostmorais@gmail.com>
2018-01-25 16:34:10 -0200

    Camera control core properties

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -1,16 +1,31 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class CameraControl : MonoBehaviour {
+public class CameraControl : MonoBehaviour
+{
+    // Inspector member objects
+    public Transform m_firstObject;
+    public Transform m_secondObject;
+    public float m_baseDistanceToAct = 10.0f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Points for distance observation
+    private Vector3 middlePoint;
+    private float distanceFromMiddlePoint;
+    private float distanceBetweenObjects;
+
+    // Camera properties
+    private float cameraDistance;
+    private float fieldOfView;
+    private float fieldOfViewTan;
+    private float aspectRatio;
+
+    void Start()
+    {
+        aspectRatio = Screen.width / Screen.height;
+        fieldOfViewTan = Mathf.Tan(Mathf.Deg2Rad * Camera.main.fieldOfView / 2.0f);
+    }
+
+    void Update()
+    {
+
+    }
 }
